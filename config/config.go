accc039cefba2ce1422f1e96ce0a6f02a012b47a
MarkTBSS <marktbss@gmail.com>
2024-02-07 09:20:17 +0700

    first commit

---
+++ config/config.go
@@ -0,0 +1,85 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadConfig(path string) IConfig {
+	envMap, err := godotenv.Read(path)
+	if err != nil {
+		log.Fatalf("load dotenv failed: %v", err)
+	}
+
+	return &config{
+		db: &db{
+			host: envMap["DB_HOST"],
+			port: func() int {
+				p, err := strconv.Atoi(envMap["DB_PORT"])
+				if err != nil {
+					log.Fatalf("load db port failed: %v", err)
+				}
+				return p
+			}(),
+			protocol: envMap["DB_PROTOCOL"],
+			username: envMap["DB_USERNAME"],
+			password: envMap["DB_PASSWORD"],
+			database: envMap["DB_DATABASE"],
+			sslMode:  envMap["DB_SSL_MODE"],
+			maxConnections: func() int {
+				m, err := strconv.Atoi(envMap["DB_MAX_CONNECTIONS"])
+				if err != nil {
+					log.Fatalf("load db max connections failed: %v", err)
+				}
+				return m
+			}(),
+		},
+	}
+}
+
+type IConfig interface {
+	Db() IDbConfig
+}
+
+type config struct {
+	db *db
+}
+
+type IDbConfig interface {
+	Url() string
+	MaxOpenConns() int
+}
+
+func (c *config) Db() IDbConfig {
+	return c.db
+}
+
+func (d *db) Url() string {
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		d.host,
+		d.port,
+		d.username,
+		d.password,
+		d.database,
+		d.sslMode,
+	)
+}
+
+func (d *db) MaxOpenConns() int {
+	return d.maxConnections
+}
+
+type db struct {
+	host           string
+	port           int
+	protocol       string
+	username       string
+	password       string
+	database       string
+	sslMode        string
+	maxConnections int
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/MarkTBSS/go-connectDatabase/config"
+	"github.com/MarkTBSS/go-connectDatabase/pkg/databases"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+func main() {
+	cfg := config.LoadConfig(envPath())
+	db := databases.DbConnect(cfg.Db())
+	fmt.Println(db)
+}
